6dd64df27ba7829ea05b0160c7797bea736e23f9
chenmengjie <chenmengjie@buaa.edu.cn>
2019-11-03 14:56:16 +0800

    [ADD][tickets]添加单线程测试内部状态

--- chapter-up-and-running/src/test/scala/aia/testdriven/SilentActor01Test.scala
+++ chapter-up-and-running/src/test/scala/aia/testdriven/SilentActor01Test.scala
@@ -1,7 +1,7 @@
 package aia.testdriven
 
-import akka.actor.{Actor, ActorSystem}
-import akka.testkit.TestKit
+import akka.actor.{Actor, ActorRef, ActorSystem}
+import akka.testkit.{TestActorRef, TestKit}
 import org.scalatest.{MustMatchers, WordSpecLike}
 
 /**
@@ -22,11 +22,41 @@ class SilentActor01Test extends TestKit(ActorSystem("testsystem")) with WordSpec
     "change state when it receives a message,multi-threaded" in {
       fail("not implemented yet")
     }
+
+    //    单线程测试内部状态
+    "change internal state when it receives a message,single" in {
+      //导入消息
+      import SilentActor._
+
+      //      为单线程测试创建
+      val silentActor = TestActorRef[SilentActor]
+      //向actor发送消息
+      silentActor ! SilentMessage("whisper")
+      //获取底层actor并检查其状态
+      silentActor.underlyingActor.state must contain("whisper")
+    }
   }
 }
 
+object SilentActor {
+
+  case class SilentMessage(data: String)
+
+  case class GetState(receiver: ActorRef)
+
+}
+
 class SilentActor extends Actor {
+
+  import SilentActor._
+
+  var internalState = Vector[String]()
+
   override def receive: Receive = {
-    case msg =>
+    //    状态保留在向量中，每条消息报都添加到这个向量中
+    case SilentMessage(data) => internalState = internalState :+ data
   }
+
+  //  返回构件好的向量的状态方法
+  def state = internalState
 }
